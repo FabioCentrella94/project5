23d27a9d890c52bf255262e8c8068af8b727040e
fabiocentrella1994@gmail.com <fabiocentrella1994@gmail.com>
2020-03-09 17:37:05 +0000

    promises implemented

--- displaysalelist.js
+++ displaysalelist.js
@@ -1,48 +1,55 @@
-let apiRequest = new XMLHttpRequest();
-
-apiRequest.open('GET', 'http://localhost:3000/api/teddies');
-apiRequest.send();
-
-apiRequest.onreadystatechange = () => {
-
-  if (apiRequest.readyState === 4) {
-
-    if (apiRequest.status === 400) {
-      
-      return alert('error');
-    
-    } else {
-
-      const response = JSON.parse(apiRequest.response);
-
-      for (i = 0; i < response.length; i++) { 
-        let figure = document.createElement('figure');
-        let salelist = document.getElementById('salelist');
-        salelist.appendChild(figure);  
-        let nameList = document.createElement('h2');
-        nameList.textContent = response[i].name;
-        figure.appendChild(nameList);
-        let imageList = document.createElement('img');
-        imageList.src = response[i].imageUrl;
-        figure.appendChild(imageList);
-        let figureCaption = document.createElement('figcaption');
-        figure.appendChild(figureCaption);
-        let link = document.createElement('a');
-        link.href = 'singleitem.html' + '?' + response[i]._id;
-        figureCaption.appendChild(link);
-        let viewItemButton = document.createElement('button');     
-        viewItemButton.textContent = 'View Item';
-        link.appendChild(viewItemButton); 
-      }
-
-      if (localStorage.getItem("totalitemincart") === null) {
-        let displayTotalItemInCart = document.getElementById('displaytotalitem');
-        displayTotalItemInCart.textContent = 0;
+const promise = new Promise((resolve, reject) => {
+  let apiRequest = new XMLHttpRequest();
+  apiRequest.open('GET', 'http://localhost:3000/api/teddies');
+  apiRequest.send();
+  apiRequest.onreadystatechange = () => {
+    if (apiRequest.readyState === 4) {
+      if (apiRequest.status === 200) {
+        resolve(JSON.parse(apiRequest.response));
       } else {
-        document.getElementById('displaytotalitem').textContent = localStorage.getItem('totalitemincart');
+        reject(JSON.parse(apiRequest.response))
       }
-
     }
   }
-}
+})
+
+promise.then((response) => {
+  for (i = 0; i < response.length; i++) { 
+    let figure = document.createElement('figure');
+    let salelist = document.getElementById('salelist');
+    salelist.appendChild(figure);  
+    let nameList = document.createElement('h2');
+    nameList.textContent = response[i].name;
+    figure.appendChild(nameList);
+    let imageList = document.createElement('img');
+    imageList.src = response[i].imageUrl;
+    figure.appendChild(imageList);
+    let figureCaption = document.createElement('figcaption');
+    figure.appendChild(figureCaption);
+    let link = document.createElement('a');
+    link.href = 'singleitem.html' + '?' + response[i]._id;
+    figureCaption.appendChild(link);
+    let viewItemButton = document.createElement('button');     
+    viewItemButton.textContent = 'View Item';
+    link.appendChild(viewItemButton); 
+  }  
+  if (localStorage.getItem("totalitemincart") === null) {
+    let displayTotalItemInCart = document.getElementById('displaytotalitem');
+    displayTotalItemInCart.textContent = 0;
+  } else {
+    document.getElementById('displaytotalitem').textContent = localStorage.getItem('totalitemincart');
+  }
+}).catch((error) => {
+    alert(error);
+})
+
+
+
+
+
+
+
+
+     
+  
 
--- displaysingleitem.js
+++ displaysingleitem.js
@@ -1,140 +1,138 @@
-let url = window.location.search.replace('?', '');
-
-let teddyRequest = new XMLHttpRequest();
-
-teddyRequest.open('GET', 'http://localhost:3000/api/teddies/' + url);
-teddyRequest.send();
-
-teddyRequest.onreadystatechange = () => {
-
-    if (teddyRequest.readyState === 4) {
-
-        if (teddyRequest.status === 500) {
-
-            return alert('error');
-
+const promise = new Promise((resolve, reject) => {
+    let url = window.location.search.replace('?', '');
+    let apiRequest = new XMLHttpRequest();
+    apiRequest.open('GET', 'http://localhost:3000/api/teddies/' + url);
+    apiRequest.send();
+    apiRequest.onreadystatechange = () => {
+      if (apiRequest.readyState === 4) {
+        if (apiRequest.status === 200) {
+          resolve(JSON.parse(apiRequest.response));
         } else {
-
-            const response = JSON.parse(teddyRequest.response);
-
-            let name = document.getElementById('name');
-            name.textContent = response.name;
-            let image = document.getElementById('image');
-            image.src = response.imageUrl;
-            let description = document.getElementById('description');
-            description.textContent = response.description;
-            let price = document.getElementById('price');
-            price.textContent = '$' + (response.price / 100).toFixed(2);
+          reject(JSON.parse(apiRequest.response))
+        }
+      }
+    }
+})
+  
+promise.then((response) => {
+    let name = document.getElementById('name');
+    name.textContent = response.name;
+    let image = document.getElementById('image');
+    image.src = response.imageUrl;
+    let description = document.getElementById('description');
+    description.textContent = response.description;
+    let price = document.getElementById('price');
+    price.textContent = '$' + (response.price / 100).toFixed(2);
         
-            let numbers = [1, 2, 3];
+    let numbers = [1, 2, 3];
 
-            let teddy = {
-                id: response._id,
-                image: response.imageUrl,
-                color: response.colors[0],
-                quantity: 1,
-                price: response.price
-            }
+    let teddy = {
+        id: response._id,
+        image: response.imageUrl,
+        color: response.colors[0],
+        quantity: 1,
+        price: response.price
+    }
 
-            for (i = 0; i < numbers.length; i++) { 
-                let chooseQuantity = document.createElement('p');
-                chooseQuantity.textContent = numbers[i];
-                let dropdown = document.getElementById('quantity');
-                dropdown.appendChild(chooseQuantity);
-                chooseQuantity.setAttribute('class', 'quantityoption')                         
-            }
+    for (i = 0; i < numbers.length; i++) { 
+        let chooseQuantity = document.createElement('p');
+        chooseQuantity.textContent = numbers[i];
+        let dropdown = document.getElementById('quantity');
+        dropdown.appendChild(chooseQuantity);
+        chooseQuantity.setAttribute('class', 'quantityoption')                         
+    }
 
-            let quantityOption = document.querySelectorAll('.quantityoption');
+    let quantityOption = document.querySelectorAll('.quantityoption');
 
-            for (i = 0; i < quantityOption.length; i++) {
-                quantityOption[i].addEventListener('click', ($event) => {
-                teddy.quantity = Number($event.target.textContent);
-                    let quantityDropdown = document.getElementById('quantitybutton');
-                    quantityDropdown.textContent = $event.target.textContent;                  
-                })
-            } 
+    for (i = 0; i < quantityOption.length; i++) {
+        quantityOption[i].addEventListener('click', ($event) => {
+        teddy.quantity = Number($event.target.textContent);
+            let quantityDropdown = document.getElementById('quantitybutton');
+            quantityDropdown.textContent = $event.target.textContent;                  
+        })
+    } 
 
-            for (i = 0; i < response.colors.length; i++) { 
-                let colorsOption = document.createElement('p');
-                colorsOption.textContent = response.colors[i];
-                let dropdown = document.getElementById('color');
-                dropdown.appendChild(colorsOption); 
-                colorsOption.setAttribute('class', 'choosecolors')                 
-            }
+    for (i = 0; i < response.colors.length; i++) { 
+        let colorsOption = document.createElement('p');
+        colorsOption.textContent = response.colors[i];
+        let dropdown = document.getElementById('color');
+        dropdown.appendChild(colorsOption); 
+        colorsOption.setAttribute('class', 'choosecolors')                 
+    }
 
-            let chooseColors = document.querySelectorAll('.choosecolors'); 
+    let chooseColors = document.querySelectorAll('.choosecolors'); 
         
-            for (i = 0; i < chooseColors.length; i++) {
-                let colorDrop = document.getElementById('colorbutton');
-                colorDrop.textContent = response.colors[0];
-                chooseColors[i].addEventListener('click', ($event) => {
-                    teddy.color = $event.target.textContent;
-                    colorDrop.textContent = $event.target.textContent;
-                })
-            } 
+    for (i = 0; i < chooseColors.length; i++) {
+        let colorDrop = document.getElementById('colorbutton');
+        colorDrop.textContent = response.colors[0];
+        chooseColors[i].addEventListener('click', ($event) => {
+            teddy.color = $event.target.textContent;
+            colorDrop.textContent = $event.target.textContent;
+        })
+    } 
 
-            let figCaption = document.getElementById('figcaption'); 
-            let linebreak = document.createElement("br");
-            document.getElementById('figcaption').appendChild(linebreak);
-            let addToCart = document.createElement('button');
-            document.getElementById('figcaption').appendChild(addToCart);
-            addToCart.textContent = 'Add To Cart';
-            addToCart.addEventListener('click', () => {
-                totalItemInCart();
-                addItemToCart();
-                totalCost();
-                location.reload();   
-            });
+    let figCaption = document.getElementById('figcaption'); 
+    let linebreak = document.createElement("br");
+    document.getElementById('figcaption').appendChild(linebreak);
+    let addToCart = document.createElement('button');
+    document.getElementById('figcaption').appendChild(addToCart);
+    addToCart.textContent = 'Add To Cart';
+    addToCart.addEventListener('click', () => {
+        totalItemInCart();
+        addItemToCart();
+        totalCost();
+        location.reload();   
+    });
 
-            if (localStorage.getItem("totalitemincart") === null) {
-                let displayTotalItemInCart = document.getElementById('displaytotalitem');
-                displayTotalItemInCart.textContent = 0;
-            } else {
-                document.getElementById('displaytotalitem').textContent = localStorage.getItem('totalitemincart');
-            }
+    if (localStorage.getItem("totalitemincart") === null) {
+        let displayTotalItemInCart = document.getElementById('displaytotalitem');
+        displayTotalItemInCart.textContent = 0;
+    } else {
+        document.getElementById('displaytotalitem').textContent = localStorage.getItem('totalitemincart');
+    }
 
-            const totalItemInCart = () => {
-                let totalItem = localStorage.getItem('totalitemincart');
-                totalItem = parseInt(totalItem);
-                if (totalItem) {
-                    localStorage.setItem('totalitemincart', totalItem + teddy.quantity);
-                } else {
-                    localStorage.setItem('totalitemincart', teddy.quantity);
-                }
-            }
-
-            const addItemToCart = () => {
-                itemInCart = localStorage.getItem(response._id);
-                itemInCart = JSON.parse(itemInCart);
-                if (itemInCart !== null ) {
-                    if (itemInCart[teddy.color] == undefined) {
-                        teddy.quantity /= 2;
-                        itemInCart = {
-                            ...itemInCart,
-                            [teddy.color] : teddy
-                        }              
-                    }
-
-                itemInCart[teddy.color].quantity += teddy.quantity; 
-
-                } else {
-                    itemInCart = {
-                        [teddy.color] : teddy 
-                    }          
-                }
-
-                localStorage.setItem(response._id, JSON.stringify(itemInCart));  
-            }
-
-            const totalCost = () => {
-                let cartCost = localStorage.getItem('totalcost');
-                if (cartCost != null) {
-                    cartCost = parseInt(cartCost);
-                    localStorage.setItem('totalcost', cartCost + (teddy.price * teddy.quantity));
-                } else {
-                    localStorage.setItem('totalcost', teddy.price * teddy.quantity);
-                }
-            }
+    const totalItemInCart = () => {
+        let totalItem = localStorage.getItem('totalitemincart');
+        totalItem = parseInt(totalItem);
+        if (totalItem) {
+            localStorage.setItem('totalitemincart', totalItem + teddy.quantity);
+        } else {
+            localStorage.setItem('totalitemincart', teddy.quantity);
         }
     }
-}
+
+    const addItemToCart = () => {
+        itemInCart = localStorage.getItem(response._id);
+        itemInCart = JSON.parse(itemInCart);
+        if (itemInCart !== null ) {
+            if (itemInCart[teddy.color] == undefined) {
+                teddy.quantity /= 2;
+                itemInCart = {
+                    ...itemInCart,
+                    [teddy.color] : teddy
+                }              
+            }
+
+        itemInCart[teddy.color].quantity += teddy.quantity; 
+
+        } else {
+            itemInCart = {
+                [teddy.color] : teddy 
+            }          
+        }
+
+        localStorage.setItem(response._id, JSON.stringify(itemInCart));  
+    }
+
+    const totalCost = () => {
+        let cartCost = localStorage.getItem('totalcost');
+        if (cartCost != null) {
+            cartCost = parseInt(cartCost);
+            localStorage.setItem('totalcost', cartCost + (teddy.price * teddy.quantity));
+        } else {
+            localStorage.setItem('totalcost', teddy.price * teddy.quantity);
+        }
+    }
+}).catch((error) =>{
+    alert(error);
+})
